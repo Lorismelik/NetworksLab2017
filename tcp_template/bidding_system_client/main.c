f5f295d3ba2d748b6f9bc9ba7ffff24ea5fd83ce
Timur Erniyazov <lorismelik@yandex.ru>
2017-11-12 23:37:23 +0300

    async client

--- tcp_template/bidding_system_client/main.c
+++ tcp_template/bidding_system_client/main.c
@@ -5,42 +5,67 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #define SRV_IP "127.0.0.1"
 #define PORT 5001 
 #define BUF_SIZE 1000
 
-void SentErr (char *s) 
+void SentErr (char *s) //error handling
 {
     perror(s);
     exit(1);
 }
 
+void *SendHandler(void* socket) {
+    int s = (int) socket;
+    int rc;
+    while (1) {
+    char *text;
+            text =(char*) malloc (sizeof(char));
+            if (text == NULL)
+                 SentErr("Can't malloc");
+            //Getting text from keyboard
+            gets(text);
+	    rc = send( s, text, 20, 0 );
+	    if ( rc <= 0 )
+                 SentErr("Sent call error");
+    }
+    
+}
+
 int main( void )
 {
-    int flag_no_read=0;
     struct sockaddr_in peer;
     int s;
     int rc;
     char buf[ BUF_SIZE ];
-
+    //Fill sockaddr_in
     peer.sin_family = AF_INET;
     peer.sin_port = htons( PORT );
     peer.sin_addr.s_addr = inet_addr( SRV_IP );
+    //we set up sockaddr_in
     if (inet_aton(SRV_IP, &peer.sin_addr)==0)
     {
         fprintf(stderr, "inet_aton() failed\n");
         exit (1);
     }
-
+    //Get socket
     s = socket( AF_INET, SOCK_STREAM, 0 );
     if ( s < 0 )
         SentErr("Socket call failed");
-
+    //Making connection
     rc = connect( s, ( struct sockaddr * )&peer, sizeof( peer ) );
     if ( rc )
         SentErr("Connect call failed");
-
+    
+    pthread_attr_t threadAttr;
+    pthread_attr_init(&threadAttr);
+    pthread_attr_setdetachstate(&threadAttr, PTHREAD_CREATE_DETACHED);
+    pthread_t send_thread;
+    //Working with forum
+    
+    rc = pthread_create(&send_thread, &threadAttr, SendHandler, (void*) s);
     while (1)
     {
         rc = recv( s, buf, BUF_SIZE, 0 );
@@ -51,12 +76,11 @@ int main( void )
             int i=0;
             while (buf[i]!=NULL)
             {
-                if (buf[0]=='^')
+                if (buf[0]=='^')//If we doing something wrong
                 {
                     printf("Invalid choose.Press enter and Try again\n");
-                    flag_no_read =1;
                 }
-                if (buf[0]=='#')
+                if (buf[0]=='#')//If we was disconected
                 {
                     printf("Closing connection...\n");
                     return 0;
@@ -66,18 +90,6 @@ int main( void )
             }
              printf("\n____________________\n");
         }
-        if (flag_no_read==0)
-        {
-            char *text;
-            text =(char*) malloc (sizeof(char));
-            if (text == NULL)
-                 SentErr("Can't malloc");
-            gets(text);
-	    rc = send( s, text, 20, 0 );
-	    if ( rc <= 0 )
-                 SentErr("Sent call error");
-        }
-        flag_no_read=0;
     }
     return 0;
 }
--- tcp_template/bidding_system_serv/main.c
+++ tcp_template/bidding_system_serv/main.c
@@ -35,14 +35,15 @@ int threads = -1; //threads counter
 bool manager_count = false; // if manager online
 char themes_names[BUF_SIZE]; //lot names
 //Comand list
-char kill_comand[] = "kill";
-char online_comand[] = "online";
+char kill_command[] = "kill";
+char online_command[] = "online";
+char shutdown_command[] = "shutdown";
 
 struct clients {
     char login[BUF_SIZE];
     int ip;
     int port;
-    int s1;
+    int s1; // socket for correctly specify the name
     bool manager; // if client is manager
 } *users;
 
@@ -55,7 +56,8 @@ int main(void) {
 
     users = (char*) malloc(sizeof (char));
     if (users == NULL) {
-        perror("Memory error");
+        perror("Neydalos' videlit' pamyat'");
+        EndTrade();
         exit(1);
     }
 
@@ -67,38 +69,40 @@ int main(void) {
     local.sin_family = AF_INET;
     local.sin_port = htons(PORT);
     local.sin_addr.s_addr = htonl(INADDR_ANY);
-
+    //make socket
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0)
         SentErrServer("Socket call failed");
-
+    //attach port
     rc = bind(s, (struct sockaddr *) &local, sizeof (local));
     if (rc < 0)
         SentErrServer("Bind call failure");
-
+    //thread options
     pthread_attr_t threadAttr;
     pthread_attr_init(&threadAttr);
     pthread_attr_setdetachstate(&threadAttr, PTHREAD_CREATE_DETACHED);
-    // Server
+    //thread for server
     pthread_t server_thread;
     rc = pthread_create(&server_thread, &threadAttr, ServerHandler, (void*) NULL);
-
+    //listening socket
     rc = listen(s, 5);
     if (rc)
         SentErrServer("Listen call failed");
+    SeeTitles(); //Get names of themes
+    //waiting for clients
     while (1) {
         //get connection
         s1 = accept(s, (struct sockaddr *) &si_other, &slen);
 
         if (s1 < 0)
             SentErrServer("Accept call failed");
-
+        //Making new user struct
 
         users[threads + 1].ip = inet_ntoa(si_other.sin_addr);
         users[threads + 1].port = ntohs(si_other.sin_port);
         users[threads + 1].s1 = s1;
         printf("new socket=%d\n", (int) s1);
-
+        //New thread   
         pthread_t client_thread;
 
         rc = pthread_create(&client_thread, &threadAttr, ClientHandler, (void*) s1);
@@ -111,24 +115,26 @@ int main(void) {
 }
 
 void *ServerHandler(void* empty) {
-    char text[40]; 
+    char text[40]; //buffer
+    //Getting text from keyboard
     while (1) {
         gets(text);
-        int i = 0;
-        int right_comand = 1;
-
-        if (strstr(kill_comand, text) != NULL) {
+        if (strstr(kill_command, text) != NULL) {
             char name[] = "";
             strcat(name, &text[5]);
             printf("%s\n", name);
             if (DeleteClient(name) == 0)
                 printf("All right. %s was killed\n", name);
             else
-                printf("Bad command. Please try again\n");
+                printf("Bad comand. Please try again\n");
         }
 
-        if (strstr(online_comand, text) != NULL) {
+        if (strstr(online_command, text) != NULL) {
             WhoIsOnline(-1);
+        }
+
+        if (strstr(shutdown_command, text) != NULL) {
+            EndTrade();
         }
     }
 }
@@ -143,8 +149,8 @@ void SendToClient(int socket, char* message) {
 void *ClientHandler(void* socket) {
     printf("New user login is:\n");
     int rc;
-    char buf[ BUF_SIZE ]; 
-    char manager[ BUF_SIZE ] = "manager"; 
+    char buf[ BUF_SIZE ]; //Buffer
+    char manager[ BUF_SIZE ] = "manager"; //Buffer
     bool is_manager;
     SendToClient((int) socket, "Please type who are you: manager(type *manager* or user *type any name*?:"); //Asking login
     //recive login
@@ -170,11 +176,12 @@ void *ClientHandler(void* socket) {
             pthread_exit(NULL);
         }
     }
+    //saving login
     int j = 0;
     for (j; j <= threads; j++) {
         if (users[j].s1 == (int) socket) {
             if (is_manager == true) {
-                if (manager_count == false)
+                if (manager_count == false)//save manager
                 {
                     users[j].manager == true;
                     manager_count = true;
@@ -195,7 +202,6 @@ void *ClientHandler(void* socket) {
         switch (pick) {
             case '0':
             {
-zero_menu_send:
                 SendToClient((int) socket, "Hello!\nWhat do you want?\n"
                         "1.See lot titles\n"
                         "2.New lot *only for manager*\n"
@@ -208,11 +214,11 @@ zero_menu_send:
             }
             case '1'://See lot titles
             {
+                memset(buf, 0, BUF_SIZE);
                 char out[BUF_SIZE] = "If you want to open lot, type name of the lot\n";
-                char name[BUF_SIZE] = {NULL};
                 strcat(out, themes_names);
-                SendToClient((int) socket, out); 
-                rc = recv((int) socket, buf, BUF_SIZE, 0); 
+                SendToClient((int) socket, out); //send lot names
+                rc = recv((int) socket, buf, BUF_SIZE, 0); //Reading new point of menu
                 if (rc <= 0)
                     SentErrServer("Recv call failed");
                 //If client want to back
@@ -221,61 +227,55 @@ zero_menu_send:
                     break;
                 }
 
+                char filename[] = "";
+                strcat(filename, buf);
+                strcat(filename, ".txt");
 
-                {
-                    char filename[] = "";
-                    strcat(filename, buf);
-                    strcat(filename, ".txt");
-
-                    if (SeeMessages(filename, (int) socket) == 1) {
-                        goto zero_menu_send;
-                    }
-                    char buf2[ BUF_SIZE ]; 
-                    rc = recv((int) socket, buf2, BUF_SIZE, 0); 
-                    if (rc <= 0)
-                        SentErrServer("Recv call failed");
-                    if (buf2[0] == '0') 
-                        goto zero_menu_send;
-
-                    else {
-                        WriteMessages(filename, (int) socket, buf2);
-                        goto zero_menu_send;
-                    }
+                if (SeeMessages(filename, (int) socket) == 1) {
+                    pick = '0';
+                    break;
+                }
+                char buf2[ BUF_SIZE ]; //New buffer only for writing message
+                rc = recv((int) socket, buf2, BUF_SIZE, 0); //Reading new message to write
+                if (rc <= 0)
+                    SentErrServer("Recv call failed");
+                if ((buf2[0] == '0') && (strlen(buf2) == 1)) { //If client want to back
+                    pick = '0';
+                    break;
+                } else {
+                    WriteMessages(filename, (int) socket, buf2);
+                    pick = '0';
+                    break;
                 }
             }
             case '2'://New lot
             {
+                memset(buf, 0, BUF_SIZE);
                 char name[ BUF_SIZE ];
                 char price[ BUF_SIZE ];
 
-                if (is_manager == false)
-                    goto zero_menu_send;
-
+                if (is_manager == false) {
+                    pick = '0';
+                    break;
+                }
                 SendToClient((int) socket, "Please write name of the lot\n");
                 rc = recv((int) socket, name, BUF_SIZE, 0);
                 if (rc <= 0)
                     SentErrServer("Recv call failed");
-                printf("%s name\n", name);
 
                 SendToClient((int) socket, "Please write price of the lot\n");
                 rc = recv((int) socket, price, BUF_SIZE, 0);
                 if (rc <= 0)
                     SentErrServer("Recv call failed");
-                printf("%s price\n", price);
-                printf("%s %s name\n", name, price);
                 NewLot(name, price, (int) socket);
-                goto zero_menu_send;
+                pick = '0';
                 break;
 
             }
             case '3'://see online users
             {
                 WhoIsOnline((int) socket);
-                //Get new point of menu
-                rc = recv((int) socket, buf, BUF_SIZE, 0);
-                if (rc <= 0)
-                    SentErrServer("Recv call failed");
-                pick = buf[0];
+                pick = '0';
                 break;
             }
             case '4'://Disconnect client
@@ -286,26 +286,27 @@ zero_menu_send:
                 SendToClient((int) socket, "#");
                 int a = DeleteClient(FindNameBySocket((int) socket));
                 pthread_exit(NULL);
+                pick = '0';
                 break;
             }
             case '5'://See result
             {
-                if (is_manager == false)
-                    goto zero_menu_send;
+                if (is_manager == false) {
+                    break;
+                }
                 SendResults();
                 EndTrade();
 
             }
-            default:
+            default://if client type illegal point in main menu
             {
-                printf("lol");
                 SendErrorToClient((int) socket);
                 pick = '0';
-                buf[0] == '0';
             }
         }
+        memset(buf, 0, BUF_SIZE);
     }
-   
+    //Disconnect client
     printf("Disconnect client");
     printf("\n");
     threads--;
@@ -323,13 +324,15 @@ int DeleteClient(char name[]) {
     printf("DeleteClient running...\n");
     int number;
     number = FindNumberByName(name);
+    //printf("%d!\n",number);
     if (number != -1) {
         if (users[number].manager == true)
             manager_count = false;
-
+        //Send signal for client
         SendToClient(users[number].s1, "#");
-
-        if (number != threads) 
+        //moving cells  
+        //printf("%d,%d\n",number, threads);
+        if (number != threads) //if it's not the last thread
         {
             users[number] = users[threads];
             memset(&users[threads], NULL, sizeof (users[threads]));
@@ -365,6 +368,7 @@ void WhoIsOnline(int socket) {
     if (socket == -1) {
         char out[BUF_SIZE] = "You want to see online users:\n";
         int i = 0;
+        //Show logins
         for (i; i <= threads; i++) {
             strcat(out, users[i].login);
             strcat(out, " ");
@@ -379,6 +383,7 @@ void WhoIsOnline(int socket) {
             strcat(out, users[i].login);
             strcat(out, " ");
         }
+        //Send logins
         SendToClient(socket, out);
     }
 }
@@ -390,8 +395,10 @@ void SeeTitles() {
     ssize_t read;
     FILE *fp;
     fp = fopen(TITLES, "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
+    if (fp == NULL) {
+        EndTrade();
+        exit(1);
+    }
     while ((read = getline(&line, &len, fp)) != -1) {
         strcat(themes_names, line);
     }
@@ -436,29 +443,33 @@ void WriteMessages(char filename[], int socket, char buf[]) {
     int i = 0;
     int j = 0;
 
-
+    //printf("%s!\n",buf);
     while (buf[i] != NULL) {
         allright = 0;
         j = 0;
         for (j; j <= 9; j++) {
- 
+            //printf ("buf=%c number =%c\n", buf[i], numbers[j]);
             if (buf[i] == numbers[j]) {
+                //printf("allright=1\n");
                 allright = 1;
             }
         }
 
         if (allright == 0) {
+            //printf("ena=0\n");
             enable = 0;
         }
-
+        // printf ("%d\n",i);
         i++;
     }
 
     if (enable == 1) {
         FILE *fp1;
         fp1 = fopen(filename, "r");
-        if (fp1 == NULL)
+        if (fp1 == NULL) {
+            EndTrade();
             exit(1);
+        }
         size_t len = 0;
         ssize_t read;
         char *line = NULL;
@@ -496,8 +507,10 @@ void WriteMessages(char filename[], int socket, char buf[]) {
 
             FILE *fp;
             fp = fopen(filename, "a");
-            if (fp == NULL)
+            if (fp == NULL) {
+                EndTrade();
                 exit(1);
+            }
             //printf("%s",filename);
             fprintf(fp, "--%s<%s> %s\n", buf, FindNameBySocket(socket), li);
             fclose(fp);
@@ -520,6 +533,7 @@ char *FindNameBySocket(int socket) {
 void SentErrServer(char *s) //error handling
 {
     perror(s);
+    EndTrade();
     exit(1);
 }
 
@@ -531,12 +545,15 @@ void NewLot(char *name, char *price, int socket) {
 
     FILE *fp1;
     fp1 = fopen(TITLES, "a");
-    if (fp1 == NULL)
+    if (fp1 == NULL) {
+        EndTrade();
         exit(1);
+    }
     fprintf(fp1, "%s\n", name);
     fclose(fp1);
     SeeTitles(); //refresh
-
+    //write message
+    //For local time
     char li[50];
     time_t rawtime;
     struct tm * timeinfo;
@@ -546,9 +563,11 @@ void NewLot(char *name, char *price, int socket) {
 
     FILE *fp2;
     fp2 = fopen(file_name, "w");
-    if (fp2 == NULL)
+    if (fp2 == NULL) {
+        EndTrade();
         exit(1);
-
+    }
+    //printf("%s",filename);
     fprintf(fp2, "--%s<%s> %s\n", price, FindNameBySocket(socket), li);
     fclose(fp2);
     SendToClient(socket, "All right!\n Hello!\nWhat are you want?\n"
@@ -563,8 +582,10 @@ void SendResults() {
     int i = 0;
     FILE *fp1;
     fp1 = fopen(TITLES, "r");
-    if (fp1 == NULL)
-        exit(EXIT_FAILURE);
+    if (fp1 == NULL) {
+        EndTrade();
+        exit(1);
+    }
     int j = 0;
     for (j; j <= threads; j++) {
 
@@ -578,8 +599,12 @@ void SendResults() {
                 str[i - 2] = NULL;
 
                 char* lot_name = str;
+                // strcat(lot_name,line1);
 
                 strcat(str, ".txt");
+
+
+                // printf("filename=%s\n",name);
 
                 size_t len = 0;
                 ssize_t read;
@@ -587,8 +612,10 @@ void SendResults() {
 
                 FILE *fp;
                 // perror(j+'0');
-                if ((fp = fopen(str, "r")) == NULL)
-                    return 1;
+                if ((fp = fopen(str, "r")) == NULL) {
+                    EndTrade();
+                    exit(1);
+                }
                 while ((read = getline(&line, &len, fp)) != -1) {
                 }
 
@@ -620,8 +647,7 @@ void SendResults() {
                     strcat(out, last_price);
                     strcat(out, "!\n");
                     strcat(result, out);
-                }
-                else {
+                } else {
 
                     char out_looser[BUF_SIZE] = "You lose lot ";
                     strcat(out_looser, lot_name);
